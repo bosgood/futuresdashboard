b7fb05605017c93e59b20f4b8a4d41721359489a
bosgood <bosgood@gmail.com>
2014-08-03 21:55:00 -0500

    Ticks to price conversion bug fix

--- app/scripts/helpers/prices.js
+++ app/scripts/helpers/prices.js
@@ -1,3 +1,5 @@
+var numeral1 = require('numeral');
+
 class PriceUtils {
   getActualValue(product, price) {
     return price * product.multiplier;
@@ -11,10 +13,14 @@ class PriceUtils {
   }
 
   convertDecimalToTicks(product, decimalPrice) {
-    decimal = decimalPrice % 1;
-    integer = decimalPrice - decimal;
-    [numerator, denominator] = product.ticksFormat.split('/');
-    fraction = decimal * denominator;
+    var decimal = decimalPrice % 1;
+    var integer = decimalPrice - decimal;
+    var [numerator, denominator] = product.ticksFormat.split('/');
+    var fraction = numeral(
+        (decimal * parseInt(denominator, 10) / 100)
+      )
+      .format('.00')
+      .slice(1, 3);
     return `${integer}'${fraction}`;
   }
 }
