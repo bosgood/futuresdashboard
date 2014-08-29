08038367b71caacb6c93c8602ede487f4df214c4
bosgood <bosgood@gmail.com>
2014-08-28 19:41:23 -0500

    Reformatting

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,38 @@
 'use strict';
 
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
-var source = require('vinyl-source-stream');
-var browserify = require('browserify');
-var es6ify = require('es6ify');
-var reactify = require('reactify');
+var gulp        = require('gulp');
+var $           = require('gulp-load-plugins')();
+var source      = require('vinyl-source-stream');
+var browserify  = require('browserify');
+var es6ify      = require('es6ify');
+var reactify    = require('reactify');
+var nconf       = require('nconf');
+var runSequence = require('run-sequence');
 
 // Styles
 gulp.task('styles', function() {
   return gulp.src('app/styles/main.scss')
-    .pipe($.rubySass({
-      style: 'expanded',
-      precision: 10,
-      loadPath: ['app/bower_components']
-    }))
-    .pipe($.autoprefixer('last 1 version'))
-    .pipe(gulp.dest('dist/styles'))
-    .pipe($.size())
-    .pipe($.connect.reload());
+  .pipe($.rubySass({
+    style: 'expanded',
+    precision: 10,
+    loadPath: ['app/bower_components']
+  }))
+  .pipe($.autoprefixer('last 1 version'))
+  .pipe(gulp.dest('dist/styles'))
+  .pipe($.size())
+  .pipe($.connect.reload());
 });
 
 // CoffeeScript
 gulp.task('coffee', function() {
   return gulp.src(
-      ['app/scripts/**/*.coffee', '!app/scripts/**/*.js'],
-      {base: 'app/scripts'}
-    )
-    .pipe(
-      $.coffee({ bare: true }).on('error', $.util.log)
-    )
-    .pipe(gulp.dest('app/scripts'));
+    ['app/scripts/**/*.coffee', '!app/scripts/**/*.js'],
+    {base: 'app/scripts'}
+  )
+  .pipe(
+    $.coffee({ bare: true }).on('error', $.util.log)
+  )
+  .pipe(gulp.dest('app/scripts'));
 });
 
 // Scripts
@@ -44,14 +46,16 @@ gulp.task('scripts', function() {
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('dist/scripts'))
     // .pipe($.size())
-    .pipe($.connect.reload());
-  });
+    .pipe($.connect.reload())
+  ;
+});
 
 gulp.task('jade', function() {
   return gulp.src('app/template/*.jade')
     .pipe($.jade({ pretty: true }))
     .pipe(gulp.dest('dist'))
-    .pipe($.connect.reload());
+    .pipe($.connect.reload())
+  ;
 });
 
 // HTML
@@ -60,7 +64,8 @@ gulp.task('html', function() {
     .pipe($.useref())
     .pipe(gulp.dest('dist'))
     .pipe($.size())
-    .pipe($.connect.reload());
+    .pipe($.connect.reload())
+  ;
 });
 
 // Images
@@ -73,21 +78,29 @@ gulp.task('images', function() {
     })))
     .pipe(gulp.dest('dist/images'))
     .pipe($.size())
-    .pipe($.connect.reload());
+    .pipe($.connect.reload())
+  ;
 });
 
 // Clean
 gulp.task('clean', function() {
-  return gulp.src(['dist/styles', 'dist/scripts', 'dist/images'], {read: false}).pipe($.clean());
+  return gulp.src(
+    ['dist/styles', 'dist/scripts', 'dist/images'],
+    {read: false})
+    .pipe($.clean())
+  ;
 });
 
 // Bundle
 gulp.task('bundle', ['styles', 'scripts', 'bower'], function() {
   return gulp.src('./app/*.html')
-         .pipe($.useref.assets())
-         .pipe($.useref.restore())
-         .pipe($.useref())
-         .pipe(gulp.dest('dist'));
+    .pipe($.useref.assets())
+    .pipe($.rev())
+    .pipe($.useref.restore())
+    .pipe($.useref())
+    .pipe($.revReplace())
+    .pipe(gulp.dest('dist'))
+  ;
 });
 
 // Build
@@ -95,7 +108,7 @@ gulp.task('build', ['html', 'bundle', 'images']);
 
 // Default task
 gulp.task('default', ['clean'], function() {
-  gulp.start('build');
+  return gulp.start('build');
 });
 
 // Connect
@@ -107,14 +120,19 @@ gulp.task('connect', $.connect.server({
 
 // Bower helper
 gulp.task('bower', function() {
-  gulp.src('app/bower_components/**/*.js', {base: 'app/bower_components'})
-    .pipe(gulp.dest('dist/bower_components/'));
-
+  return gulp.src(
+    'app/bower_components/**/*.js',
+    {base: 'app/bower_components'})
+      .pipe(gulp.dest('dist/bower_components/'))
+  ;
 });
 
 gulp.task('json', function() {
-  gulp.src('app/scripts/json/**/*.json', {base: 'app/scripts'})
-    .pipe(gulp.dest('dist/scripts/'));
+  return gulp.src(
+    'app/scripts/json/**/*.json',
+    {base: 'app/scripts'})
+      .pipe(gulp.dest('dist/scripts/'))
+  ;
 });
 
 // Watch
